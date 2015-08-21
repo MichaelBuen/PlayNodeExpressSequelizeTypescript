7b406f9d69b21d3e6d25736e4cd66b995f46c1e3
MichaelBuen <me@ienablemuch.com>
2015-08-21 10:44:20 +0800

    use path.join

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ Api(app);
 // Serving static files
 // http://stackoverflow.com/questions/4720343/loading-basic-html-in-node-js
 // http://stackoverflow.com/questions/16593686/what-is-the-best-practice-for-serving-html-in-node-js-with-express-js
-app.use('/', express.static(__dirname + '/public', { extensions: ['html'] })); // if entered a url without an extension, attach html
-app.use('/angular', express.static(path.join(__dirname, '/node_modules/angular')));
-app.use('/domains', express.static(path.join(__dirname, '/domains')));
-app.use('/functionalities', express.static(path.join(__dirname, '/functionalities'), { extensions: ['html'] }));
+app.use('/', express.static(path.join(__dirname, 'public'), { extensions: ['html'] })); // if entered a url without an extension, attach html
+app.use('/angular', express.static(path.join(__dirname, 'node_modules', 'angular')));
+app.use('/domains', express.static(path.join(__dirname, 'domains')));
+app.use('/functionalities', express.static(path.join(__dirname, 'functionalities'), { extensions: ['html'] }));
 var server = app.listen(3000, function () {
     var host = server.address().address;
     var port = server.address().port;
